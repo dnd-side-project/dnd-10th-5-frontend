183283c86bc380d54cccf0450ce3c9e7ad3554f0
Wonsuk Choi <sukvvon@gmail.com>
2024-01-30 00:53:03 +0900

    chore(eslint-config-favolink): @typescript-eslint/strict-type-checked, @stylistic 추가 (#15)
    * chore(package.json(eslint-config-favolink)): @stylistic/eslint-plugin 추가
    
    * chore(eslint-config-favolink): @typescript-eslint/strict-type-checked, @stylistic 추가
    
    * chore(pnpm-lock): @stylistic/eslint-plugin 추가
    
    * style(packages/ui): 수정된 eslint --fix 적용

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -12,7 +12,9 @@ export function Button({ children, className, appName }: ButtonProps) {
   return (
     <button
       className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
+      onClick={() => {
+        alert(`Hello from your ${appName} app!`);
+      }}
     >
       {children}
     </button>
