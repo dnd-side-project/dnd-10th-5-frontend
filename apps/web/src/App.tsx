c31e74c533f375480dd9cecf4157a0fa87fb6203
Wonsuk Choi <sukvvon@gmail.com>
2024-02-13 08:44:03 +0900

    feat(web): reset.css.ts 생성 및 적용, 기존 css 파일 제거

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,8 +1,8 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
-import './App.css';
 import { router } from './routes';
+import '@/styles/reset.css';
 
 export default function App() {
   const queryClient = new QueryClient({
