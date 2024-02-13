818948a43303a23e8c7cfbd4c0b0ef3ed0e3f5f4
lake.zero(레이크/한영수) <lake.zero@kakaostyle.com>
2024-02-13 09:55:08 +0900

    feat(packages): util에 fetcher 추가 (#33)
    * feat(packages): util에 fetcher 추가
    
    * feat: 코드리뷰 반영
    
    * chore: remove console
    
    * feat: extention에도 process.env 사용하도록 설정
    
    * feat(packages): patch생성
    
    * feat(packages): vite 환경변수 사용하도록 수정
    
    * feat: web에서 baseUrl 설정하도록 수정
    
    * chore: change timeout

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,8 +1,11 @@
+import { setAxiosUrl } from '@favolink/utils';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import { router } from './routes';
+
+setAxiosUrl(import.meta.env.VITE_API_HOST as string);
 
 export default function App() {
   const queryClient = new QueryClient({
---
+++ packages/shared/utils/src/request.ts
@@ -0,0 +1,56 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
+});
+
+export function setAxiosUrl(url: string) {
+  axiosInstance.defaults.baseURL = url;
+}
+
+async function request<Data, Result>(
+  url: string,
+  method: string,
+  data?: Record<string, Data>,
+) {
+  const response = await axiosInstance({
+    url,
+    method,
+    data,
+  });
+
+  return response.data as Result;
+}
+
+export async function get<Result>(url: string) {
+  return request<undefined, Result>(url, 'GET');
+}
+
+export async function post<Data, Result>(
+  url: string,
+  data: Record<string, Data>,
+) {
+  return request<Data, Result>(url, 'POST', data);
+}
+
+export async function put<Data, Result>(
+  url: string,
+  data: Record<string, Data>,
+) {
+  return request<Data, Result>(url, 'PUT', data);
+}
+
+export async function del<Result>(url: string) {
+  return request<undefined, Result>(url, 'DELETE');
+}
+
+export async function patch<Data, Result>(
+  url: string,
+  data: Record<string, Data>,
+) {
+  return request<Data, Result>(url, 'PATCH', data);
+}
