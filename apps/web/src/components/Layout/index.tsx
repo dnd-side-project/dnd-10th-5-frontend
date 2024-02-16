244a54c9e77ee6a7d091bc422788378630b5a6ad
Wonsuk Choi <sukvvon@gmail.com>
2024-02-17 08:42:51 +0900

    feat(web/components/Layout): HOME에 접속시 SAVED_LINK로 이동하도록 설정

--- apps/web/src/components/Layout/index.tsx
+++ apps/web/src/components/Layout/index.tsx
@@ -1,8 +1,16 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
 import { Header, NavBar } from './components';
 import Wrapper from '../Wrapper';
+import { PATH } from '@/constants';
 
 export default function Layout() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate(PATH.SAVED_LINK, { replace: true });
+  }, [navigate]);
+
   return (
     <>
       <Header />
