bb003f149280ec10b210ad4d879518be7f1ad71d
Wonsuk Choi <sukvvon@gmail.com>
2024-02-17 07:57:34 +0900

    feat(web/App.tsx): router 추가

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -2,7 +2,7 @@ import { setAxiosUrl } from '@favolink/utils';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
-import { router } from './routes';
+import { router } from './router';
 import '@favolink/styles/reset.css';
 import '@/styles/font.css';
 import '@favolink/styles/theme.css';
