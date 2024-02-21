143a15f4b595c110354a126e0bd5f35e008fbbef
Wonsuk Choi <sukvvon@gmail.com>
2024-02-22 04:57:26 +0900

    feat(web/components): Portal 컴포넌트 추가

--- apps/web/src/components/index.tsx
+++ apps/web/src/components/index.tsx
@@ -1,3 +1,4 @@
 /* eslint-disable @stylistic/padding-line-between-statements */
 export { default as Layout } from './Layout';
+export { default as Portal } from './Portal';
 export { default as Wrapper } from './Wrapper';
