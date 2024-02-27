8906d14bccae7ac03ec070b5881799e7e1344f7c
Wonsuk Choi <sukvvon@gmail.com>
2024-02-28 00:00:33 +0900

    feat(web/components): Portal, Modal 컴포넌트 구성에서 제거

--- apps/web/src/components/index.tsx
+++ apps/web/src/components/index.tsx
@@ -1,5 +1,3 @@
 /* eslint-disable @stylistic/padding-line-between-statements */
 export { default as Layout } from './Layout';
-export { default as Modal } from './Modal';
-export { default as Portal } from './Portal';
 export { default as Wrapper } from './Wrapper';
