cfb90a757559fead79368399e966fef348f9c753
Wonsuk Choi <sukvvon@gmail.com>
2024-02-28 23:30:58 +0900

    fix(shared/components): Toast, favolinkProvider 컴포넌트 index에서 제거

--- packages/shared/components/src/index.tsx
+++ packages/shared/components/src/index.tsx
@@ -4,5 +4,3 @@ export { default as Button } from './Button';
 export { default as Heading } from './Heading';
 export { default as Modal } from './Modal';
 export { default as Portal } from './Portal';
-export { default as FavolinkProvider } from './Provider';
-export { default as Toast } from './Toast';
