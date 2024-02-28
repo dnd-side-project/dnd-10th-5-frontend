eb59797246c31a26d2c81b75e411f72c2238890c
Wonsuk Choi <sukvvon@gmail.com>
2024-02-28 23:36:37 +0900

    feat(shared/{components,hooks}): Heading 컴포넌트 추가, useDisclosure 훅 이동 (#64)
    * feat(web/components): Heading 컴포넌트 추가, index에 Modal, Portal 컴포넌트 추가
    
    * feat(shared/hooks): useDisclosure 훅 추가
    
    * fix(shared/components): Toast, favolinkProvider 컴포넌트 index에서 제거

--- apps/web/src/hooks/index.ts
+++
@@ -1,2 +0,0 @@
-/* eslint-disable @stylistic/padding-line-between-statements */
-export { default as useDisclosure } from './useDisclosure';
--- packages/shared/components/src/index.tsx
+++ packages/shared/components/src/index.tsx
@@ -1,2 +1,6 @@
-/* eslint-disable @stylistic/padding-line-between-statements */
+/* eslint-disable @stylistic/padding-line-between-statements, react-refresh/only-export-components */
+export * from '@favolink/hooks';
 export { default as Button } from './Button';
+export { default as Heading } from './Heading';
+export { default as Modal } from './Modal';
+export { default as Portal } from './Portal';
