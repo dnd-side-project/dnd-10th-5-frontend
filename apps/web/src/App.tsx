d7f466fdb7f499e99df9a8b4148c3f44f995ccea
Wonsuk Choi <sukvvon@gmail.com>
2024-03-06 19:59:15 +0900

    feat(web/App.tsx): FavolinkProvider 추가

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,3 +1,4 @@
+import { FavolinkProvider } from '@favolink/react';
 import { setAxiosUrl } from '@favolink/utils';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
@@ -20,8 +21,10 @@ export default function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <ReactQueryDevtools initialIsOpen={false} />
+      <FavolinkProvider>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </FavolinkProvider>
     </QueryClientProvider>
   );
 }
