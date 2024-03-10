bd7be1229aa15729ecbdd29b51457cfbe8d09400
Wonsuk Choi <sukvvon@gmail.com>
2024-03-11 01:00:03 +0900

    style(shared/components): index.tsx에서 index.ts로 변경

---
+++ packages/shared/components/src/index.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @stylistic/padding-line-between-statements, react-refresh/only-export-components */
+export * from '@favolink/hooks';
+export * from '@favolink/system';
+export { default as Box } from './Box';
+export { default as Button, type ButtonProps } from './Button';
+export { default as Heading, type HeadingProps } from './Heading';
+export { default as Link, type LinkProps } from './Link';
+export { default as Modal } from './Modal';
+export { default as Portal } from './Portal';
+export { default as FavolinkProvider } from './Provider';
+export { default as Text, type TextProps } from './Text';
+export { default as Toast } from './Toast';
