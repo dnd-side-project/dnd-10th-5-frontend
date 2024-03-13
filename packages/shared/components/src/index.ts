34120e1f3bf56c91ad0ad47902ef74a3b05d821c
Wonsuk Choi <sukvvon@gmail.com>
2024-03-13 23:48:16 +0900

    refactor(shared/components/Modal): 합성 컴포넌트 제거, favolink factory, system/createContext 사용 (#85)

--- packages/shared/components/src/index.ts
+++ packages/shared/components/src/index.ts
@@ -5,7 +5,23 @@ export { default as Box } from './Box';
 export { default as Button, type ButtonProps } from './Button';
 export { default as Heading, type HeadingProps } from './Heading';
 export { default as Link, type LinkProps } from './Link';
-export { default as Modal } from './Modal';
+export {
+  default as Modal,
+  ModalBody,
+  type ModalBodyProps,
+  ModalContent,
+  type ModalContentProps,
+  ModalFooter,
+  type ModalFooterProps,
+  ModalHeader,
+  type ModalHeaderProps,
+  ModalOverlay,
+  type ModalOverlayProps,
+  ModalTitle,
+  type ModalTitleProps,
+  ModalTopBar,
+  type ModalTopBarProps,
+} from './Modal';
 export { default as Portal } from './Portal';
 export { default as FavolinkProvider } from './Provider';
 export { default as Text, type TextProps } from './Text';
