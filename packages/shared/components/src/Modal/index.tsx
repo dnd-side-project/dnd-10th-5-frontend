c9f9b4774e42e71f0231f9544bcbbd7ef010720b
Wonsuk Choi <sukvvon@gmail.com>
2024-03-13 23:42:33 +0900

    refactor(shared/components/Modal): 합성 컴포넌트 제거, favolink factory, system/createContext 사용

--- packages/shared/components/src/Modal/index.tsx
+++ packages/shared/components/src/Modal/index.tsx
@@ -1,132 +1,133 @@
-import { forwardRef } from '@favolink/system';
-import { classNames } from '@favolink/utils';
 import {
-  type ComponentPropsWithoutRef,
-  type ReactNode,
+  type ContextProviderProps,
   createContext,
-  useContext,
-} from 'react';
+  favolink,
+  forwardRef,
+} from '@favolink/system';
+import { classNames } from '@favolink/utils';
+import { type ComponentPropsWithoutRef } from 'react';
 import * as styles from './styles.css';
-import Box from '../Box';
 import Heading, { type HeadingProps } from '../Heading';
 import Portal from '../Portal';
 import Text from '../Text';
 
 const MODAL_CLASSNAME = 'favolink-modal';
 
-type OverlayProps = Omit<ComponentPropsWithoutRef<'div'>, 'className'> & {
+export type ModalOverlayProps = Omit<
+  ComponentPropsWithoutRef<'div'>,
+  'className'
+> & {
   variant?: styles.OverlayVariant;
 };
 
-const Overlay = forwardRef<OverlayProps, 'div'>(function Overlay(props, ref) {
-  const { variant = 'original', ...restProps } = props;
+export const ModalOverlay = forwardRef<ModalOverlayProps, 'div'>(
+  function ModalOverlay(props, ref) {
+    const { variant = 'original', ...restProps } = props;
 
-  return (
-    <Box
-      {...restProps}
-      ref={ref}
-      className={classNames(
-        `${MODAL_CLASSNAME}__overlay`,
-        styles.overlay[variant],
-      )}
-    />
-  );
-});
-
-Modal.Overlay = Overlay;
-
-type ContentProps = ComponentPropsWithoutRef<'div'>;
-
-const Content = forwardRef<ContentProps, 'div'>(function Content(props, ref) {
-  const { children, className, ...restPorps } = props;
-
-  const { onClose, closeOnOverlayClick } = useContext(ModalContext);
-
-  return (
-    <>
-      <Overlay
-        onClick={closeOnOverlayClick ? onClose : undefined}
-        variant="withContent"
-      />
-      <Box
-        {...restPorps}
+    return (
+      <favolink.div
+        {...restProps}
         ref={ref}
         className={classNames(
-          `${MODAL_CLASSNAME}__content`,
-          styles.content,
+          `${MODAL_CLASSNAME}__overlay`,
+          styles.overlay[variant],
+        )}
+      />
+    );
+  },
+);
+
+export type ModalContentProps = ComponentPropsWithoutRef<'div'>;
+
+export const ModalContent = forwardRef<ModalContentProps, 'div'>(
+  function ModalContent(props, ref) {
+    const { children, className, ...restPorps } = props;
+
+    const { onClose, closeOnOverlayClick } = useModalContext();
+
+    return (
+      <>
+        <ModalOverlay
+          onClick={closeOnOverlayClick ? onClose : undefined}
+          variant="withContent"
+        />
+        <favolink.div
+          {...restPorps}
+          ref={ref}
+          className={classNames(
+            `${MODAL_CLASSNAME}__content`,
+            styles.content,
+            className,
+          )}
+        >
+          {children}
+        </favolink.div>
+      </>
+    );
+  },
+);
+
+export type ModalHeaderProps = ComponentPropsWithoutRef<'header'>;
+
+export const ModalHeader = forwardRef<ModalHeaderProps, 'header'>(
+  function ModalHeader(props, ref) {
+    const { children, className, ...restProps } = props;
+
+    return (
+      <favolink.header
+        {...restProps}
+        ref={ref}
+        className={classNames(
+          `${MODAL_CLASSNAME}__header`,
+          styles.header,
           className,
         )}
       >
         {children}
-      </Box>
-    </>
-  );
-});
+      </favolink.header>
+    );
+  },
+);
 
-Modal.Content = Content;
-
-type HeaderProps = ComponentPropsWithoutRef<'header'>;
-
-const Header = forwardRef<HeaderProps, 'header'>(function Header(props, ref) {
-  const { children, className, ...restProps } = props;
-
-  return (
-    <Box
-      as="header"
-      {...restProps}
-      ref={ref}
-      className={classNames(
-        `${MODAL_CLASSNAME}__header`,
-        styles.header,
-        className,
-      )}
-    >
-      {children}
-    </Box>
-  );
-});
-
-Modal.Header = Header;
-
-type TopBarProps = ComponentPropsWithoutRef<'div'> & {
+export type ModalTopBarProps = ComponentPropsWithoutRef<'div'> & {
   layout: styles.TopBarLayout;
 };
 
-const TopBar = forwardRef<TopBarProps, 'div'>(function TopBar(props, ref) {
-  const { children, layout, className, ...restProps } = props;
+export const ModalTopBar = forwardRef<ModalTopBarProps, 'div'>(
+  function ModalTopBar(props, ref) {
+    const { children, layout, className, ...restProps } = props;
 
-  const { onClose } = useContext(ModalContext);
+    const { onClose } = useModalContext();
 
-  const isCouple = layout === 'couple';
+    const isCouple = layout === 'couple';
 
-  return (
-    <Box
-      {...restProps}
-      ref={ref}
-      className={classNames(
-        `${MODAL_CLASSNAME}__topbar`,
-        styles.topBarLayout[layout],
-        className,
-      )}
-    >
-      {isCouple && <Text>{children}</Text>}
-      <Text onClick={onClose}>닫기</Text>
-    </Box>
-  );
-});
+    return (
+      <favolink.div
+        {...restProps}
+        ref={ref}
+        className={classNames(
+          `${MODAL_CLASSNAME}__topbar`,
+          styles.topBarLayout[layout],
+          className,
+        )}
+      >
+        {isCouple && <Text>{children}</Text>}
+        <Text onClick={onClose}>닫기</Text>
+      </favolink.div>
+    );
+  },
+);
 
-Modal.TopBar = TopBar;
-
-type TitleProps = HeadingProps & {
+export type ModalTitleProps = HeadingProps & {
   layout: styles.TitleLayout;
 };
 
-const Title = forwardRef<TitleProps, typeof Heading>(
-  function Title(props, ref) {
+export const ModalTitle = forwardRef<ModalTitleProps, typeof Heading>(
+  function ModalTitle(props, ref) {
     const { children, className, layout, ...restProps } = props;
 
     return (
-      <Box className={styles.titleLayout[layout]}>
+      <favolink.div className={styles.titleLayout[layout]}>
         <Heading
           as="h4"
           {...restProps}
@@ -139,80 +140,64 @@ const Title = forwardRef<TitleProps, typeof Heading>(
         >
           {children}
         </Heading>
-      </Box>
+      </favolink.div>
     );
   },
 );
 
-Modal.Title = Title;
+export type ModalBodyProps = ComponentPropsWithoutRef<'main'>;
 
-type BodyProps = ComponentPropsWithoutRef<'main'>;
+export const ModalBody = forwardRef<ModalBodyProps, 'main'>(
+  function ModalBody(props, ref) {
+    const { children, className, ...restProps } = props;
 
-const Body = forwardRef<BodyProps, 'main'>(function Body(props, ref) {
-  const { children, className, ...restProps } = props;
+    return (
+      <favolink.main
+        as="main"
+        {...restProps}
+        ref={ref}
+        className={classNames(`${MODAL_CLASSNAME}__body`, className)}
+      >
+        {children}
+      </favolink.main>
+    );
+  },
+);
 
-  return (
-    <Box
-      as="main"
-      {...restProps}
-      ref={ref}
-      className={classNames(`${MODAL_CLASSNAME}__body`, className)}
-    >
-      {children}
-    </Box>
-  );
-});
+export type ModalFooterProps = ComponentPropsWithoutRef<'footer'>;
 
-Modal.Body = Body;
+export const ModalFooter = forwardRef<ModalFooterProps, 'footer'>(
+  function ModalFooter(props, ref) {
+    const { children, className, ...restProps } = props;
 
-type FooterProps = ComponentPropsWithoutRef<'footer'>;
+    return (
+      <favolink.footer
+        {...restProps}
+        ref={ref}
+        className={classNames(
+          `${MODAL_CLASSNAME}__footer`,
+          styles.footer,
+          className,
+        )}
+      >
+        {children}
+      </favolink.footer>
+    );
+  },
+);
 
-const Footer = forwardRef<FooterProps, 'footer'>(function Footer(props, ref) {
-  const { children, className, ...restProps } = props;
-
-  return (
-    <Box
-      as="footer"
-      {...restProps}
-      ref={ref}
-      className={classNames(
-        `${MODAL_CLASSNAME}__footer`,
-        styles.footer,
-        className,
-      )}
-    >
-      {children}
-    </Box>
-  );
-});
-
-Modal.Footer = Footer;
-
-type ModalContextOptions = {
+type ModalContextDefaultValue = {
   onClose: () => void;
   closeOnOverlayClick?: boolean;
 };
 
-const ModalContext = createContext<ModalContextOptions>({
-  onClose: () => {},
-  closeOnOverlayClick: false,
-});
+const [ModalProvider, useModalContext] =
+  createContext<ModalContextDefaultValue>({
+    onClose: () => {},
+    closeOnOverlayClick: false,
+  });
 
-type ModalProviderProps = ModalContextOptions & {
-  children: ReactNode;
-};
-
-function Provider(props: ModalProviderProps) {
-  const { children, onClose, closeOnOverlayClick } = props;
-
-  return (
-    <ModalContext.Provider value={{ onClose, closeOnOverlayClick }}>
-      {children}
-    </ModalContext.Provider>
-  );
-}
-
-type ModalProps = ModalProviderProps & {
+type ModalProps = ContextProviderProps<ModalContextDefaultValue> & {
   isOpen: boolean;
 };
 
@@ -223,7 +208,7 @@ export default function Modal(props: ModalProps) {
   return (
     isOpen && (
       <Portal type="modal">
-        <Provider {...realRestProps}>{children}</Provider>
+        <ModalProvider {...realRestProps}>{children}</ModalProvider>
       </Portal>
     )
   );
