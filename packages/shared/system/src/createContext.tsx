6dd573346a7a5fd68de8ad5cc11e56190b581e69
Wonsuk Choi <sukvvon@gmail.com>
2024-03-13 20:59:56 +0900

    feat(shared/system): createContext 함수 추가

---
+++ packages/shared/system/src/createContext.tsx
@@ -0,0 +1,46 @@
+import {
+  type Context,
+  type ReactNode,
+  createContext as createReactContext,
+  useContext as useReactContext,
+} from 'react';
+
+export type ContextProviderProps<ContextDefaultValue> = ContextDefaultValue & {
+  children: ReactNode;
+};
+
+type CreateContextReturn<CreateContextDefaultValue> = [
+  (props: ContextProviderProps<CreateContextDefaultValue>) => JSX.Element,
+  () => CreateContextDefaultValue,
+  Context<CreateContextDefaultValue>,
+];
+
+export default function createContext<CreateContextDefaultValue>(
+  defaultValue: CreateContextDefaultValue,
+) {
+  const Context = createReactContext<CreateContextDefaultValue>(defaultValue);
+
+  function useContext() {
+    const context = useReactContext(Context);
+
+    return context;
+  }
+
+  function ContextProvider(
+    props: ContextProviderProps<CreateContextDefaultValue>,
+  ) {
+    const { children, ...restProps } = props;
+
+    return (
+      <Context.Provider value={restProps as CreateContextDefaultValue}>
+        {children}
+      </Context.Provider>
+    );
+  }
+
+  return [
+    ContextProvider,
+    useContext,
+    Context,
+  ] as CreateContextReturn<CreateContextDefaultValue>;
+}
