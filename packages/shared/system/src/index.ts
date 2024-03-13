d1417fe1967041f09bcebd50dcc7ac209c2ff2f0
Wonsuk Choi <sukvvon@gmail.com>
2024-03-13 21:04:26 +0900

    feat(shared/system): createContext 함수 추가 (#83)

--- packages/shared/system/src/index.ts
+++ packages/shared/system/src/index.ts
@@ -1,5 +1,9 @@
 /* eslint-disable @stylistic/padding-line-between-statements */
 export { default as createComponent } from './createComponent';
+export {
+  default as createContext,
+  type ContextProviderProps,
+} from './createContext';
 export { default as createIcon } from './createIcon';
 export * from './factory';
 export { default as forwardRef } from './forwardRef';
