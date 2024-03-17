db28448b3fa206459e15383ce9751728154c8d5d
Wonsuk Choi <sukvvon@gmail.com>
2024-03-18 02:38:58 +0900

    feat(web/Layout/Header/styles.css.ts): theme.css.ts 수정사항 반영

--- apps/web/src/components/Layout/components/Header/styles.css.ts
+++ apps/web/src/components/Layout/components/Header/styles.css.ts
@@ -5,7 +5,7 @@ import { style } from '@vanilla-extract/css';
 export const header = style([
   h1SemiBold,
   {
-    color: vars.color.archiveCoral,
-    backgroundColor: vars.color.archiveYellow,
+    color: vars.color.coral,
+    backgroundColor: vars.color.yellow,
   },
 ]);
