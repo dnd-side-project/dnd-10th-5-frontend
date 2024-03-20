01e7fcf0a1dadec070d06dd8dbcdb5bfa1197bbc
Wonsuk Choi <sukvvon@gmail.com>
2024-03-20 22:23:29 +0900

    feat(shared/components): Input 및 관련 컴포넌트 추가 (#109)
    * feat(shared/components/Input): styles.css.ts 추가
    
    * feat(shared/components/Input): Input 및 관련 컴포넌트 추가
    
    * feat(shared/components/index.ts): Input 관련 components, types 추가
    
    * feat(shared/components/Input): Input withElement 관련 널 병합 연산자로 변경

---
+++ packages/shared/components/src/Input/index.tsx
@@ -0,0 +1,98 @@
+import { favolink, forwardRef } from '@favolink/system';
+import { classNames } from '@favolink/utils';
+import { type ComponentPropsWithoutRef } from 'react';
+import * as styles from './styles.css';
+
+const INPUT_CLASSNAME = 'favolink-input';
+
+export type InputGroupProps = ComponentPropsWithoutRef<'div'>;
+
+export const InputGroup = forwardRef<InputGroupProps, 'div'>(
+  function InputGroup(props, ref) {
+    const { children, className, ...restProps } = props;
+
+    return (
+      <favolink.div
+        {...restProps}
+        ref={ref}
+        className={classNames(
+          `${INPUT_CLASSNAME}__group`,
+          styles.groupBase,
+          className,
+        )}
+      >
+        {children}
+      </favolink.div>
+    );
+  },
+);
+
+type InputElementProps = ComponentPropsWithoutRef<'div'>;
+
+export type InputLeftElementProps = InputElementProps;
+
+export const InputLeftElement = forwardRef<InputLeftElementProps, 'div'>(
+  function InputElement(props, ref) {
+    const { children, className, ...restProps } = props;
+
+    return (
+      <favolink.div
+        {...restProps}
+        ref={ref}
+        className={classNames(
+          `${INPUT_CLASSNAME}__left-element`,
+          styles.elementDirection.left,
+          className,
+        )}
+      >
+        {children}
+      </favolink.div>
+    );
+  },
+);
+
+export type InputRightElementProps = InputElementProps;
+
+export const InputRightElement = forwardRef<InputRightElementProps, 'div'>(
+  function InputElement(props, ref) {
+    const { children, className, ...restProps } = props;
+
+    return (
+      <favolink.div
+        {...restProps}
+        ref={ref}
+        className={classNames(
+          `${INPUT_CLASSNAME}__right-element`,
+          styles.elementDirection.right,
+          className,
+        )}
+      >
+        {children}
+      </favolink.div>
+    );
+  },
+);
+
+export type InputProps = ComponentPropsWithoutRef<'input'> & {
+  variant?: styles.Variant;
+};
+
+const Input = forwardRef<InputProps, 'input'>(function Input(props, ref) {
+  const { className, variant = 'outline', ...restPorps } = props;
+
+  return (
+    <favolink.input
+      {...restPorps}
+      ref={ref}
+      className={classNames(
+        INPUT_CLASSNAME,
+        styles.base,
+        styles.variant[variant],
+        className ?? styles.withElement,
+        className,
+      )}
+    />
+  );
+});
+
+export default Input;
