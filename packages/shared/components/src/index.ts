d5f3641dbdf728bbafc72f083fce47bf6345b389
Wonsuk Choi <sukvvon@gmail.com>
2024-03-24 20:45:39 +0900

    refactor(shared/components/index.ts): 파일마다 전체(*) export로 변경

--- packages/shared/components/src/index.ts
+++ packages/shared/components/src/index.ts
@@ -1,47 +1,15 @@
 /* eslint-disable @stylistic/padding-line-between-statements, react-refresh/only-export-components */
 export * from '@favolink/hooks';
 export * from '@favolink/system';
-export { default as Box } from './Box';
-export { default as Button, type ButtonProps } from './Button';
-export { default as Heading, type HeadingProps } from './Heading';
-export { default as Icon, type IconProps } from './Icon';
-export {
-  default as Input,
-  type InputProps,
-  InputGroup,
-  type InputGroupProps,
-  InputLeftElement,
-  type InputLeftElementProps,
-  InputRightElement,
-  type InputRightElementProps,
-} from './Input';
-export { default as Link, type LinkProps } from './Link';
-export {
-  default as Modal,
-  ModalBody,
-  type ModalBodyProps,
-  ModalContent,
-  type ModalContentProps,
-  ModalFooter,
-  type ModalFooterProps,
-  ModalHeader,
-  type ModalHeaderProps,
-  ModalOverlay,
-  type ModalOverlayProps,
-  ModalTitle,
-  type ModalTitleProps,
-  ModalTopBar,
-  type ModalTopBarProps,
-} from './Modal';
-export { default as Portal } from './Portal';
-export { default as FavolinkProvider } from './Provider';
-export {
-  type TagProps,
-  default as Tag,
-  TagIcon,
-  type TagIconProps,
-  TagLabel,
-  type TagLabelProps,
-} from './Tag';
-export { default as Text, type TextProps } from './Text';
-export { default as Toast, type ToastProps, ToastProvider } from './Toast';
+export * from './Box';
+export * from './Button';
+export * from './Heading';
+export * from './Icon';
+export * from './Input';
+export * from './Link';
+export * from './Modal';
+export * from './Portal';
+export * from './Provider';
+export * from './Tag';
+export * from './Text';
+export * from './Toast';
