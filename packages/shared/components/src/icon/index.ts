48f2745c16abb3853ab99ce9b3d12f771db6252d
Wonsuk Choi <sukvvon@gmail.com>
2024-03-28 17:53:28 +0900

    refactor(shared/components/icon): icon 컴포넌트 위치 변경, create-icon.ts 추가, index.ts에 추가

---
+++ packages/shared/components/src/icon/index.ts
@@ -0,0 +1,3 @@
+/* eslint-disable @stylistic/padding-line-between-statements, react-refresh/only-export-components */
+export { createIcon } from './create-Icon';
+export { Icon, type IconProps } from './icon';
