8ecc0309a07ced88123016c59054d4cd306a9c6b
Wonsuk Choi <sukvvon@gmail.com>
2024-03-28 16:04:27 +0900

    feat(shared/system): createIcon 함수 system에서 제거

--- packages/shared/system/src/index.ts
+++ packages/shared/system/src/index.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @stylistic/padding-line-between-statements */
 export * from './createComponent';
 export * from './createContext';
-export * from './createIcon';
 export * from './factory';
 export * from './forwardRef';
 export * from './types';
