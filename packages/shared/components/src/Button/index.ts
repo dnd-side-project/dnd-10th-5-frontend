f8ef020325631e44453db37aee57be68c081a71a
Wonsuk Choi <sukvvon@gmail.com>
2024-03-28 17:38:59 +0900

    feat(shared/components/button): index.ts에 icon-button 컴포넌트 관련 모듈 추가

--- packages/shared/components/src/Button/index.ts
+++ packages/shared/components/src/Button/index.ts
@@ -1,2 +1,3 @@
 /* eslint-disable @stylistic/padding-line-between-statements, react-refresh/only-export-components */
 export { Button, type ButtonProps } from './button';
+export { IconButton, type IconButtonProps } from './icon-button';
