540b18686378fa2319ef0b3d5ef6e819b71635b4
Wonsuk Choi <sukvvon@gmail.com>
2024-03-31 19:57:40 +0900

    feat(shared/components/toast): index.ts에 useToast 추가

--- packages/shared/components/src/toast/index.ts
+++ packages/shared/components/src/toast/index.ts
@@ -1,3 +1,4 @@
 /* eslint-disable @stylistic/padding-line-between-statements, react-refresh/only-export-components */
 export { Toast, type ToastProps } from './toast';
 export { ToastProvider } from './toast.provider';
+export { useToast } from './use-toast';
