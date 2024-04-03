8adb31e4db539a8ccc31244dcf572bccf52da35c
Wonsuk Choi <sukvvon@gmail.com>
2024-04-03 15:55:20 +0900

    refactor(shared/components/icon/create-icon): 통일성을 위해 svg에서 typeof Icon으로 변경

--- packages/shared/components/src/icon/create-Icon.tsx
+++ packages/shared/components/src/icon/create-Icon.tsx
@@ -12,7 +12,7 @@ export function createIcon(options: CreateIconOptions) {
   const { d: pathDefinition, path, defaultProps = {} } = options;
   const { viewBox = '0 0 20 20', ...restDefaultProps } = defaultProps;
 
-  const Component = forwardRef<IconProps, 'svg'>(
+  const Component = forwardRef<IconProps, typeof Icon>(
     function Component(props, ref) {
       const pathElements = Children.toArray(path);
 
