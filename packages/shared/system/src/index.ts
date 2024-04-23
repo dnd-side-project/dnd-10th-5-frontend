bafb463438595cd990fe785f3a18abf3241dd09a
Wonsuk Choi <sukvvon@gmail.com>
2024-04-23 18:03:56 +0900

    feat(shared/system): StyleProps 타입, createRawStyleProps 함수 추가 (#156)

--- packages/shared/system/src/index.ts
+++ packages/shared/system/src/index.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @stylistic/padding-line-between-statements */
 export * from './create-component';
 export * from './create-context';
+export * from './create-raw-style-props';
 export * from './factory';
 export * from './forward-ref';
 export * from './types';
