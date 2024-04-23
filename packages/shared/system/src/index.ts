467df015a65c1df0c285cfc9ac56511933bac9da
Wonsuk Choi <sukvvon@gmail.com>
2024-04-23 22:29:28 +0900

    feat(shared/system): extractProps 함수 추가 (#157)

--- packages/shared/system/src/index.ts
+++ packages/shared/system/src/index.ts
@@ -2,6 +2,7 @@
 export * from './create-component';
 export * from './create-context';
 export * from './create-raw-style-props';
+export * from './extract-props';
 export * from './factory';
 export * from './forward-ref';
 export * from './types';
