7302b771dcbadbeba1590fdde1d28c906a60a309
Wonsuk Choi <sukvvon@gmail.com>
2024-04-27 22:30:35 +0900

    refactor(shared/components/layout/flex): Flex 컴포넌트에 extractProps 추가, flex.styles.css.ts 내부 flexStyleVars 사용 (#166)
    * refactor(shared/components/layout/flex.styles.css.ts): 기존 var들을 flexStyleVars로 변경
    
    * refactor(shared/components/layout/flex.tsx): 구조 분해에서 extractProps을 활용하는 것으로 변경

--- packages/shared/components/src/layout/flex.styles.css.ts
+++ packages/shared/components/src/layout/flex.styles.css.ts
@@ -1,26 +1,7 @@
-import { createVar, style } from '@vanilla-extract/css';
-
-export const flexJustify = createVar();
-
-export const flexAlign = createVar();
-
-export const flexWrap = createVar();
-
-export const flexDirection = createVar();
-
-export const flexBasis = createVar();
-
-export const flexShrink = createVar();
-
-export const flexGrow = createVar();
+import { flexStyleVars } from '@favolink/styles';
+import { style } from '@vanilla-extract/css';
 
 export const flexBase = style({
   display: 'flex',
-  justifyContent: flexJustify,
-  alignItems: flexAlign,
-  flexWrap,
-  flexDirection,
-  flexBasis,
-  flexShrink,
-  flexGrow,
+  ...flexStyleVars,
 });
--- packages/shared/components/src/layout/flex.tsx
+++ packages/shared/components/src/layout/flex.tsx
@@ -1,53 +1,31 @@
-import { type HTMLFavolinkProps, favolink, forwardRef } from '@favolink/system';
+import {
+  type FlexStyleProps,
+  flexStyleProps,
+  flexStyleVars,
+} from '@favolink/styles';
+import {
+  type HTMLFavolinkProps,
+  extractProps,
+  favolink,
+  forwardRef,
+} from '@favolink/system';
 import { cx } from '@favolink/utils';
-import { assignInlineVars } from '@vanilla-extract/dynamic';
-import { type CSSProperties } from 'react';
 import * as styles from './flex.styles.css';
 
-type FlexOptions = {
-  justify?: CSSProperties['justifyContent'];
-  align?: CSSProperties['alignItems'];
-  wrap?: CSSProperties['flexWrap'];
-  direction?: CSSProperties['flexDirection'];
-  grow?: CSSProperties['flexGrow'];
-  shrink?: CSSProperties['flexShrink'];
-  basis?: CSSProperties['flexBasis'];
-};
-
-export type FlexProps = FlexOptions & HTMLFavolinkProps<'div'>;
+export type FlexProps = FlexStyleProps & HTMLFavolinkProps<'div'>;
 
 export const Flex = forwardRef<FlexProps, 'div'>(function Flex(props, ref) {
-  const {
-    children,
-    className,
-    style,
-    justify,
-    align,
-    wrap,
-    direction,
-    grow,
-    shrink,
-    basis,
-    ...restProps
-  } = props;
+  const { children, className, ...restProps } = extractProps(
+    props,
+    flexStyleVars,
+    flexStyleProps,
+  );
 
   return (
     <favolink.div
       {...restProps}
       ref={ref}
       className={cx(styles.flexBase, className)}
-      style={{
-        ...assignInlineVars({
-          [styles.flexJustify]: justify,
-          [styles.flexAlign]: align,
-          [styles.flexWrap]: wrap,
-          [styles.flexDirection]: direction,
-          [styles.flexShrink]: String(shrink),
-          [styles.flexBasis]: String(basis),
-          [styles.flexGrow]: String(grow),
-        }),
-        ...style,
-      }}
     >
       {children}
     </favolink.div>
