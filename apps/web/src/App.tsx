7be98d19287e8d510353253b7d070617fdc21d2c
Wonsuk Choi <sukvvon@gmail.com>
2024-04-29 21:06:53 +0900

    refactor(apps/web/App.tsx): @favolink-ui/react로 변경사항 반영

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,12 +1,11 @@
-import { FavolinkProvider } from '@favolink/react';
-import { setAxiosUrl } from '@favolink/utils';
+import { FavolinkProvider, setAxiosUrl } from '@favolink-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
-import '@favolink/styles/reset.css';
+import '@favolink/react/reset.css';
+import '@favolink/react/theme.css';
 import '@/styles/font.css';
-import '@favolink/styles/theme.css';
 
 setAxiosUrl(import.meta.env.VITE_API_HOST as string);
 
