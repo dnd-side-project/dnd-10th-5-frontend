5c7ef08eef73f932589fb00fb01e70258e1febbf
Wonsuk Choi <sukvvon@gmail.com>
2024-05-02 22:13:51 +0900

    feat(ui/react/icons): BellIcon 추가

---
+++ packages/ui/react/src/icons/src/bell.tsx
@@ -0,0 +1,18 @@
+import { createIcon } from './icon';
+
+export const BellIcon = createIcon({
+  defaultProps: {
+    width: '1em',
+    height: '1em',
+    fill: 'none',
+  },
+  path: (
+    <path
+      d="M12.7084 12.5849C12.934 13.8507 12.2917 16 10 16C7.70833 16 7.06679 13.8507 7.2924 12.5849M10 4C8.625 4 7.4959 4.39728 6.79167 5.71429C5.875 7.42857 7.25 10 4.5 12.5714H15.5C12.75 10 14.125 7.42857 13.2083 5.71429C12.5041 4.39728 11.375 4 10 4Z"
+      stroke="currentColor"
+      strokeWidth="1.27"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  ),
+});
