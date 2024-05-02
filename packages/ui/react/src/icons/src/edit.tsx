40f3c25a4a44ee9469a8522fe9917128dd7c50f7
Wonsuk Choi <sukvvon@gmail.com>
2024-05-02 22:31:03 +0900

    feat(ui/react/icons): GNB 관련 icon들 추가 (#169)
    * feat(ui/react/icons): AddIcon 추가
    
    * feat(ui/react/icons): BellIcon 추가
    
    * feat(ui/react/icons): CancelIcon 추가
    
    * feat(ui/react/icons): ChevronDownIcon 추가
    
    * feat(ui/react/icons): ChevronLeftIcon 추가
    
    * feat(ui/react/icons): ChevronRightIcon 추가
    
    * feat(ui/react/icons): ChevronUpIcon 추가
    
    * feat(ui/react/icons): DrawerIcon 추가
    
    * feat(ui/react/icons): EditIcon 추가
    
    * feat(ui/react/icons): HeartIcon 추가
    
    * feat(ui/react/icons): MenuIcon 추가
    
    * feat(ui/react/icons): MoreHorizIcon 추가
    
    * feat(ui/react/icons): SearchIcon 추가
    
    * feat(ui/react/icons): TimerIcon 추가
    
    * feat(ui/react/icons): TrashIcon 추가
    
    * feat(ui/react/icons/index.ts): GNB 관련 아이콘들 추가

---
+++ packages/ui/react/src/icons/src/edit.tsx
@@ -0,0 +1,18 @@
+import { createIcon } from './icon';
+
+export const EditIcon = createIcon({
+  defaultProps: {
+    width: '1em',
+    height: '1em',
+    fill: 'none',
+  },
+  path: (
+    <path
+      d="M11.4706 6.34283L13.2353 8.19794M10.2941 15H15M5.58824 12.5265L5 15L7.35294 14.3816L14.1682 7.21721C14.3888 6.98528 14.5127 6.67077 14.5127 6.34283C14.5127 6.01489 14.3888 5.70038 14.1682 5.46846L14.0671 5.3621C13.8464 5.13025 13.5473 5 13.2353 5C12.9233 5 12.6242 5.13025 12.4035 5.3621L5.58824 12.5265Z"
+      stroke="currentColor"
+      strokeWidth="1.2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  ),
+});
---
+++ packages/ui/react/src/icons/src/heart.tsx
@@ -0,0 +1,17 @@
+import { createIcon } from './icon';
+
+export const HeartIcon = createIcon({
+  defaultProps: {
+    width: '1em',
+    height: '1em',
+    fill: 'none',
+  },
+  path: (
+    <path
+      d="M14.8301 5.97567C13.6868 4.84145 11.8333 4.84144 10.69 5.97567L10 6.75L9.30998 5.97568C8.16674 4.84146 6.31318 4.84146 5.16993 5.97568C4.02669 7.1099 4.02669 8.94883 5.16993 10.0831L9.99999 14.875L14.8301 10.083C15.9733 8.94883 15.9733 7.10989 14.8301 5.97567Z"
+      stroke="currentColor"
+      strokeWidth="1.27"
+      strokeLinejoin="round"
+    />
+  ),
+});
