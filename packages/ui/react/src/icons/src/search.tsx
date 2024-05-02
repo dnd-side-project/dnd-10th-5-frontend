1692bd77309f5354ae4c9ca1f463e366c7331456
Wonsuk Choi <sukvvon@gmail.com>
2024-05-02 22:21:58 +0900

    feat(ui/react/icons): SearchIcon 추가

---
+++ packages/ui/react/src/icons/src/search.tsx
@@ -0,0 +1,18 @@
+import { createIcon } from './icon';
+
+export const SearchIcon = createIcon({
+  defaultProps: {
+    width: '1em',
+    height: '1em',
+    fill: 'none',
+  },
+  path: (
+    <path
+      d="M16.25 16.25L12.676 12.635M8.95833 14.1667C6.875 14.1667 3.75 12.6042 3.75 8.95833C3.75 5.3125 6.875 3.75 8.95833 3.75C11.0417 3.75 14.1667 5.3125 14.1667 8.95833C14.1667 12.6042 11.0417 14.1667 8.95833 14.1667Z"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  ),
+});
