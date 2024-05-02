1799ce3d0612b7430abe67b55023cf135206886b
Wonsuk Choi <sukvvon@gmail.com>
2024-05-02 22:23:58 +0900

    feat(ui/react/icons/index.ts): GNB 관련 아이콘들 추가

--- packages/ui/react/src/icons/src/index.ts
+++ packages/ui/react/src/icons/src/index.ts
@@ -1,2 +1,18 @@
 /* eslint-disable @stylistic/padding-line-between-statements, react-refresh/only-export-components */
 export * from './icon';
+
+export * from './add';
+export * from './bell';
+export * from './cancel';
+export * from './chevron-down';
+export * from './chevron-left';
+export * from './chevron-right';
+export * from './chevron-up';
+export * from './drawer';
+export * from './edit';
+export * from './heart';
+export * from './menu';
+export * from './more-horiz';
+export * from './search';
+export * from './timer';
+export * from './trash';
