cd0531517b8e47bdd93041da1705e3a7f3aab4ff
Wonsuk Choi <sukvvon@gmail.com>
2024-05-03 23:30:10 +0900

    feat(ui/react/components/card): card 관련 컴포넌트들을 모은 index.ts 추가

---
+++ packages/ui/react/src/components/card/index.ts
@@ -0,0 +1,5 @@
+/* eslint-disable @stylistic/padding-line-between-statements, react-refresh/only-export-components */
+export { Card, type CardProps } from './card';
+export { CardBody, type CardBodyProps } from './card-body';
+export { CardFooter, type CardFooterProps } from './card-footer';
+export { CardHeader, type CardHeaderProps } from './card-header';
