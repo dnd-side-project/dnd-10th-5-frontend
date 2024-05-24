ac873b84c530905006814084bf50fab43a2d60ba
Wonsuk Choi <sukvvon@gmail.com>
2024-05-24 20:16:14 +0900

    feat(ui/system/forward-ref.ts): PolymorphicProps 관련 변경 적용

--- packages/ui/react/src/system/src/forward-ref.ts
+++ packages/ui/react/src/system/src/forward-ref.ts
@@ -4,17 +4,20 @@ import {
   type ForwardRefRenderFunction,
   forwardRef as ReactForwardRef,
 } from 'react';
-import { type ComponentWithAs, type RightJoinProps } from './types';
+import {
+  type ComponentWithPolymorphic,
+  type PolymorphicProps,
+  type RightJoinProps,
+} from './types';
 
 export function forwardRef<Props extends object, Component extends ElementType>(
   render: ForwardRefRenderFunction<
     any,
-    RightJoinProps<ComponentPropsWithoutRef<Component>, Props> & {
-      as?: ElementType;
-    }
+    PolymorphicProps &
+      RightJoinProps<ComponentPropsWithoutRef<Component>, Props>
   >,
 ) {
-  return ReactForwardRef(render) as unknown as ComponentWithAs<
+  return ReactForwardRef(render) as unknown as ComponentWithPolymorphic<
     Component,
     Props
   >;
