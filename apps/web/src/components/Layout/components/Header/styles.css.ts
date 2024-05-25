2693e9f6d9c784163f34ba88ecc1eeac36d0a682
Wonsuk Choi <sukvvon@gmail.com>
2024-05-25 16:11:49 +0900

    refactor(apps/web/components/Layout/Header/styles.css.ts): vars 변경사항 반영

--- apps/web/src/components/Layout/components/Header/styles.css.ts
+++ apps/web/src/components/Layout/components/Header/styles.css.ts
@@ -1,11 +1,11 @@
 import { h1SemiBold } from '@favolink-ui/react/text.css';
-import { vars } from '@favolink-ui/react/theme.css';
+import { vars } from '@favolink-ui/react/vars.css';
 import { style } from '@vanilla-extract/css';
 
 export const header = style([
   h1SemiBold,
   {
-    color: vars.color.coral,
-    backgroundColor: vars.color.yellow,
+    color: vars.color.archive.coral,
+    backgroundColor: vars.color.archive.yellow,
   },
 ]);
