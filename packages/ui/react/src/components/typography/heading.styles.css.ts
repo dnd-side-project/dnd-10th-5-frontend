9adad4745be3c470825038b6ca5d34b1370345f2
Wonsuk Choi <sukvvon@gmail.com>
2024-05-29 15:25:35 +0900

    refactor(ui/react/components/typography): heading, text recipe로 변경

--- packages/ui/react/src/components/typography/heading.styles.css.ts
+++ packages/ui/react/src/components/typography/heading.styles.css.ts
@@ -1,4 +1,4 @@
-import { styleVariants } from '@vanilla-extract/css';
+import { type RecipeVariants, recipe } from '@vanilla-extract/recipes';
 import {
   h1Bold,
   h1SemiBold,
@@ -14,19 +14,27 @@ import {
   h6SemiBold,
 } from '../../styles/text.css';
 
-export const headingWeight = styleVariants({
-  h1bold: [h1Bold],
-  h1semibold: [h1SemiBold],
-  h2bold: [h2Bold],
-  h2semibold: [h2SemiBold],
-  h3bold: [h3Bold],
-  h3semibold: [h3SemiBold],
-  h4bold: [h4Bold],
-  h4semibold: [h4SemiBold],
-  h5bold: [h5Bold],
-  h5semibold: [h5SemiBold],
-  h6bold: [h6Bold],
-  h6semibold: [h6SemiBold],
+export const heading = recipe({
+  variants: {
+    weight: {
+      h1bold: [h1Bold],
+      h1semibold: [h1SemiBold],
+      h2bold: [h2Bold],
+      h2semibold: [h2SemiBold],
+      h3bold: [h3Bold],
+      h3semibold: [h3SemiBold],
+      h4bold: [h4Bold],
+      h4semibold: [h4SemiBold],
+      h5bold: [h5Bold],
+      h5semibold: [h5SemiBold],
+      h6bold: [h6Bold],
+      h6semibold: [h6SemiBold],
+    },
+  },
+
+  defaultVariants: {
+    weight: 'h2bold',
+  },
 });
 
-export type HeadingWeight = keyof typeof headingWeight;
+export type HeadingVariants = RecipeVariants<typeof heading>;
--- packages/ui/react/src/components/typography/text.tsx
+++ packages/ui/react/src/components/typography/text.tsx
@@ -6,18 +6,16 @@ import {
 import { cx } from '@favolink-ui/utils';
 import * as styles from './text.styles.css';
 
-export type TextProps = HTMLFavolinkProps<'p'> & {
-  scale?: styles.TextScale;
-};
+export type TextProps = HTMLFavolinkProps<'p'> & styles.TextVariants;
 
 export const Text = forwardRef<TextProps, 'p'>(function Text(props, ref) {
-  const { children, className, scale = '2regular', ...restProps } = props;
+  const { children, className, scale, ...restProps } = props;
 
   return (
     <favolink.p
       {...restProps}
       ref={ref}
-      className={cx('favolink-text', styles.textScale[scale], className)}
+      className={cx('favolink-text', styles.text({ scale }), className)}
     >
       {children}
     </favolink.p>
