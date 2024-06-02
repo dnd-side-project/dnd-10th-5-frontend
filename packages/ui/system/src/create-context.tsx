aca38b2f9be20b2903fc72c3e1cf790acf0e1962
Wonsuk Choi <sukvvon@gmail.com>
2024-06-02 21:16:42 +0900

    feat(ui/system/create-context.tsx): error일 경우 추가, options 확장하며 defaultValue 선택으로 변경

--- packages/ui/system/src/create-context.tsx
+++ packages/ui/system/src/create-context.tsx
@@ -5,22 +5,61 @@ import {
   useContext as useReactContext,
 } from 'react';
 
+type CreatContextOptions<CreateContextDefaultValue> = {
+  name?: string;
+  hookName?: string;
+  providerName?: string;
+  errorEessage?: string;
+  defaultValue?: CreateContextDefaultValue;
+};
+
 type CreateContextReturn<CreateContextDefaultValue> = [
   Provider<CreateContextDefaultValue>,
   () => CreateContextDefaultValue,
   Context<CreateContextDefaultValue>,
 ];
 
+function getErrorMessage(hook: string, provider: string) {
+  return `${hook} returned \`undefined\`. Seems you forgot to wrap component within ${provider}`;
+}
+
 export function createContext<CreateContextDefaultValue>(
-  defaultValue: CreateContextDefaultValue,
-): CreateContextReturn<CreateContextDefaultValue> {
-  const Context = createReactContext<CreateContextDefaultValue>(defaultValue);
+  options: CreatContextOptions<CreateContextDefaultValue>,
+) {
+  const {
+    name,
+    hookName = 'useContext',
+    providerName = 'Provider',
+    errorEessage,
+    defaultValue,
+  } = options;
+
+  const Context = createReactContext<CreateContextDefaultValue | undefined>(
+    defaultValue,
+  );
+
+  Context.displayName = name;
 
   function useContext() {
     const context = useReactContext(Context);
 
+    if (!context) {
+      const error = new Error(
+        errorEessage ?? getErrorMessage(hookName, providerName),
+      );
+
+      error.name = 'ContextError';
+      Error.captureStackTrace(error, useContext);
+
+      throw error;
+    }
+
     return context;
   }
 
-  return [Context.Provider, useContext, Context];
+  return [
+    Context.Provider,
+    useContext,
+    Context,
+  ] as CreateContextReturn<CreateContextDefaultValue>;
 }
