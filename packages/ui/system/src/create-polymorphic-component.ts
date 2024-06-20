7cde622d6944bf2563fa687a08f4831374a715cf
Wonsuk Choi <sukvvon@gmail.com>
2024-06-20 23:25:12 +0900

    feat(ui/system/create-polymorphic-component.ts): Slottable 상황 추가

--- packages/ui/system/src/create-polymorphic-component.ts
+++ packages/ui/system/src/create-polymorphic-component.ts
@@ -3,17 +3,26 @@ import {
   Children,
   type ComponentPropsWithoutRef,
   type ElementType,
+  type ReactElement,
+  type ReactNode,
   type Ref,
   cloneElement,
   createElement,
   isValidElement,
 } from 'react';
 import { forwardRef } from './forward-ref';
+import { Slottable, type SlottableProps } from './slottable';
 import {
   type ComponentWithPolymorphic,
   type JsxElements,
   type PolymorphicProps,
 } from './types';
+
+export function isSlottable(
+  child: ReactNode,
+): child is ReactElement<SlottableProps> {
+  return isValidElement(child) && child.type === Slottable;
+}
 
 export type FavolinkComponent<Component extends ElementType> =
   ComponentWithPolymorphic<Component, object>;
@@ -34,6 +43,43 @@ export function createPolymorphicComponent<Component extends ElementType>(
           },
           children,
         );
+      }
+
+      const childrenArray = Children.toArray(children);
+      const slottable = childrenArray.find(isSlottable);
+
+      if (slottable) {
+        const newElement = slottable.props.children;
+        const newChildren = childrenArray.map((child) => {
+          if (child === slottable) {
+            if (Children.count(newElement) > 1) {
+              return Children.only(null);
+            }
+
+            return isValidElement(newElement)
+              ? ((newElement as ReactElement<{ children: ReactNode }>).props
+                  .children as ReactNode)
+              : null;
+          } else {
+            return child;
+          }
+        });
+
+        return isValidElement(newElement)
+          ? cloneElement(
+              newElement as ReactElement,
+              {
+                ...mergeProps(restProps, newElement.props),
+                ref: ref
+                  ? composeRefs(
+                      ref,
+                      (newElement as ReactElement & { ref: Ref<any> }).ref,
+                    )
+                  : (newElement as ReactElement & { ref: Ref<any> }).ref,
+              },
+              newChildren,
+            )
+          : null;
       }
 
       const onlyChild = Children.only(children);
