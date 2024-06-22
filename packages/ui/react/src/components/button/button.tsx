3efbbce5481300ffa81c9a2dd8dd1b52b5089aa1
Wonsuk Choi <sukvvon@gmail.com>
2024-06-22 22:45:12 +0900

    feat(ui/react/button/button.tsx): rightElement, leftElement를 포함한 variants 추가 적용

--- packages/ui/react/src/components/button/button.tsx
+++ packages/ui/react/src/components/button/button.tsx
@@ -1,28 +1,64 @@
 import {
   type HTMLFavolinkProps,
+  Slottable,
   favolink,
   forwardRef,
 } from '@favolink-ui/system';
 import { cx } from '@favolink-ui/utils';
+import { type ReactElement } from 'react';
 import * as styles from './button.styles.css';
 
-export type ButtonProps = HTMLFavolinkProps<'button'> & styles.ButtonVariants;
+type ExtendedButtonProps = {
+  rightElement?: ReactElement;
+  leftElement?: ReactElement;
+};
+
+export type ButtonProps = ExtendedButtonProps &
+  HTMLFavolinkProps<'button'> &
+  styles.ButtonVariants & { colorScheme?: styles.ButtonColorScheme };
 
 export const Button = forwardRef<ButtonProps, 'button'>(
   function Button(props, ref) {
-    const { children, className, colorScheme, size, ...restProps } = props;
+    const {
+      children,
+      className,
+      rightElement,
+      leftElement,
+      radius,
+      variant,
+      fullfill,
+      justify,
+      text,
+      colorScheme = 'white',
+      ...restProps
+    } = props;
+
+    const hasElement = Boolean(rightElement || leftElement);
 
     return (
       <favolink.button
+        type="button"
         {...restProps}
         ref={ref}
         className={cx(
           'favolink-button',
-          styles.button({ size, colorScheme }),
+          styles.buttonVariants({
+            radius,
+            variant,
+            fullfill,
+            justify,
+            text,
+          }),
+          styles.buttonColorScheme[colorScheme],
+          hasElement && styles.buttonHasElement,
           className,
         )}
       >
-        {children}
+        {leftElement}
+        <Slottable>{children}</Slottable>
+        <span className={cx(styles.justifyStartButtonHasRightElement)}>
+          {rightElement}
+        </span>
       </favolink.button>
     );
   },
