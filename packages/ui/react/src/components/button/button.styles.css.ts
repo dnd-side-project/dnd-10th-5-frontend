528d957f59d66e75b5d3b99b73ee7bd089330f2d
Wonsuk Choi <sukvvon@gmail.com>
2024-06-24 18:35:00 +0900

    feat(ui/react/button): Button 컴포넌트에 rightElement, leftElement를 포함한 속성들 추가 (#201)
    * feat(ui/react/button/button.styles.css.ts): button 컴포넌트 variant 추가
    
    * feat(ui/react/button/button.tsx): rightElement, leftElement를 포함한 variants 추가 적용
    
    * feat(ui/react/button): Button 컴포넌트 속성 width, rounded로 변경

--- packages/ui/react/src/components/button/button.styles.css.ts
+++ packages/ui/react/src/components/button/button.styles.css.ts
@@ -1,47 +1,119 @@
+import {
+  type ComplexStyleRule,
+  style,
+  styleVariants,
+} from '@vanilla-extract/css';
 import { type RecipeVariants, recipe } from '@vanilla-extract/recipes';
-import { h5Bold, h6SemiBold } from '../../styles/text.css';
+import { type CSSProperties } from 'react';
+import { body3Regular, h6SemiBold } from '../../styles/text.css';
 import { vars } from '../../styles/vars.css';
 
-export const button = recipe({
+function makeButtonArchiveColorScheme(
+  color: CSSProperties['color'],
+): ComplexStyleRule {
+  return {
+    selectors: {
+      [`${buttonVariant.solid}&`]: {
+        backgroundColor: color,
+        border: `1px solid ${color}`,
+        color: color === 'white' ? vars.color.gray[1100] : 'white',
+      },
+      [`${buttonVariant.outline}&`]: {
+        color: vars.color.gray[1100],
+        backgroundColor: 'inherit',
+        border: `1px solid ${color}`,
+      },
+    },
+  };
+}
+
+export const buttonVariants = recipe({
   base: {
-    borderRadius: 8,
+    display: 'inline-flex',
+    alignItems: 'center',
     cursor: 'pointer',
+    textDecoration: 'none',
+    boxSizing: 'border-box',
+    appearance: 'none',
+    verticalAlign: 'middle',
+    whiteSpace: 'nowrap',
+    padding: '8px 10px',
+    minWidth: 72,
   },
-
   variants: {
-    colorScheme: {
-      gray: {
-        backgroundColor: vars.color.gray[400],
-        border: `1px solid ${vars.color.gray[400]}`,
-        color: vars.color.gray[200],
+    justify: {
+      center: {
+        justifyContent: 'center',
       },
-      black: {
-        backgroundColor: vars.color.gray[1000],
-        border: `1px solid ${vars.color.gray[1000]}`,
-        color: 'white',
-      },
-      white: {
-        backgroundColor: 'white',
-        border: `1px solid ${vars.color.gray[300]}`,
-        color: vars.color.gray[900],
-      },
-      red: {
-        backgroundColor: vars.color.system[300],
-        border: `1px solid ${vars.color.system[300]}`,
-        color: 'white',
+      start: {
+        justifyContent: 'start',
       },
     },
-    size: {
-      small: [h6SemiBold, { minWidth: 100, minHeight: 30 }],
-      medium: [h6SemiBold, { minWidth: 202, minHeight: 40 }],
-      large: [h5Bold, { minWidth: 332, minHeight: 44 }],
+    variant: {
+      solid: {},
+      outline: {},
+    },
+    text: {
+      normal: [body3Regular],
+      strength: [h6SemiBold],
+    },
+    rounded: {
+      full: {
+        borderRadius: 9999,
+      },
+      normal: {
+        borderRadius: 8,
+      },
+    },
+    width: {
+      full: {
+        width: '100%',
+      },
     },
   },
-
   defaultVariants: {
-    colorScheme: 'white',
-    size: 'medium',
+    justify: 'center',
+    variant: 'solid',
+    text: 'normal',
+    rounded: 'normal',
+    width: undefined,
   },
 });
 
-export type ButtonVariants = RecipeVariants<typeof button>;
+export type ButtonVariants = RecipeVariants<typeof buttonVariants>;
+
+const { variant: buttonVariant } = buttonVariants.classNames.variants;
+
+export const buttonColorScheme = styleVariants({
+  archiveBlack: makeButtonArchiveColorScheme(vars.color.archive.black),
+  archiveBlue: makeButtonArchiveColorScheme(vars.color.archive.blue),
+  archiveBrightGreen: makeButtonArchiveColorScheme(
+    vars.color.archive.brightGreen,
+  ),
+  archiveCoral: makeButtonArchiveColorScheme(vars.color.archive.coral),
+  archiveMint: makeButtonArchiveColorScheme(vars.color.archive.mint),
+  archivePink: makeButtonArchiveColorScheme(vars.color.archive.mint),
+  archivePurple: makeButtonArchiveColorScheme(vars.color.archive.purple),
+  archiveYellow: makeButtonArchiveColorScheme(vars.color.archive.yellow),
+  black: makeButtonArchiveColorScheme(vars.color.gray[1000]),
+  gray: makeButtonArchiveColorScheme(vars.color.gray[400]),
+  white: makeButtonArchiveColorScheme('white'),
+  red: makeButtonArchiveColorScheme(vars.color.system[300]),
+});
+
+export type ButtonColorScheme = keyof typeof buttonColorScheme;
+
+export const buttonHasElement = style({
+  gap: 10,
+});
+
+export const justifyStartButtonHasRightElement = style({
+  display: 'inline-flex',
+  alignItems: 'center',
+  selectors: {
+    [`${buttonVariants.classNames.variants.justify.start} &`]: {
+      flexGrow: 1,
+      justifyContent: 'flex-end',
+    },
+  },
+});
