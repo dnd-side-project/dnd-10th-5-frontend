a2a98178571e3fa1bd0cd547f65de213f080bcb9
Wonsuk Choi <sukvvon@gmail.com>
2024-07-05 21:19:05 +0900

    feat(ui/react/modal): index.ts에 ModalClose, ModalTirgger, ModalProtal 추가 (#208)

--- packages/ui/react/src/components/modal/index.ts
+++ packages/ui/react/src/components/modal/index.ts
@@ -1,9 +1,12 @@
 /* eslint-disable @stylistic/padding-line-between-statements, react-refresh/only-export-components */
 export { Modal } from './modal';
 export { ModalBody, type ModalBodyProps } from './modal-body';
+export { ModalClose, type ModalCloseProps } from './modal-close';
 export { ModalContent, type ModalContentProps } from './modal-content';
 export { ModalFooter, type ModalFooterProps } from './modal-footer';
 export { ModalHeader, type ModalHeaderProps } from './modal-header';
 export { ModalOverlay, type ModalOverlayProps } from './modal-overlay';
+export { ModalPortal, type ModalPortalProps } from './modal-portal';
 export { ModalTitle, type ModalTitleProps } from './modal-title';
 export { ModalTopbar, type ModalTopbarProps } from './modal-topbar';
+export { ModalTrigger, type ModalTriggerProps } from './modal-trigger';
