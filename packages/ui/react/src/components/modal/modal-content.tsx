a07711561d913a0b41aeb336192c7e2cc05e2144
Wonsuk Choi <sukvvon@gmail.com>
2024-07-05 20:45:22 +0900

    refactor(ui/react/modal): ModalContent 내부 ModalOverlay 제거

--- packages/ui/react/src/components/modal/modal-content.tsx
+++ packages/ui/react/src/components/modal/modal-content.tsx
@@ -5,8 +5,6 @@ import {
 } from '@favolink-ui/system';
 import { cx } from '@favolink-ui/utils';
 import * as styles from './modal-content.styles.css';
-import { ModalOverlay } from './modal-overlay';
-import { useModalContext } from './modal.context';
 
 export type ModalContentProps = HTMLFavolinkProps<'div'>;
 
@@ -14,26 +12,18 @@ export const ModalContent = forwardRef<ModalContentProps, 'div'>(
   function ModalContent(props, ref) {
     const { children, className, ...restPorps } = props;
 
-    const { onClose, closeOnOverlayClick } = useModalContext();
-
     return (
-      <>
-        <ModalOverlay
-          onClick={closeOnOverlayClick ? onClose : undefined}
-          variant="withContent"
-        />
-        <favolink.div
-          {...restPorps}
-          ref={ref}
-          className={cx(
-            'favolink-modal__content',
-            styles.modalContentBase,
-            className,
-          )}
-        >
-          {children}
-        </favolink.div>
-      </>
+      <favolink.div
+        {...restPorps}
+        ref={ref}
+        className={cx(
+          'favolink-modal__content',
+          styles.modalContentBase,
+          className,
+        )}
+      >
+        {children}
+      </favolink.div>
     );
   },
 );
