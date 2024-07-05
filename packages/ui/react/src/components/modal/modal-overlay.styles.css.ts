068abeecf9476e36876c53667b1f75402cc91055
Wonsuk Choi <sukvvon@gmail.com>
2024-07-05 20:46:45 +0900

    refactor(ui/react/modal): ModalOverlay style inset으로 단축, recipe 제거

--- packages/ui/react/src/components/modal/modal-overlay.styles.css.ts
+++ packages/ui/react/src/components/modal/modal-overlay.styles.css.ts
@@ -1,32 +1,7 @@
-import { type RecipeVariants, recipe } from '@vanilla-extract/recipes';
+import { style } from '@vanilla-extract/css';
 
-export const modalOverlay = recipe({
-  base: {
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  variants: {
-    variant: {
-      original: {
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        zIndex: 96,
-      },
-      withContent: {
-        zIndex: 97,
-      },
-    },
-  },
-
-  defaultVariants: {
-    variant: 'original',
-  },
+export const modalOverlayBase = style({
+  position: 'fixed',
+  inset: 0,
+  backgroundColor: 'rgba(0, 0, 0, 0.5)',
 });
-
-export type ModalOverlayVariants = RecipeVariants<typeof modalOverlay>;
