b9cebfdbdf181ae4d38f36fdaf9541391b725b35
Wonsuk Choi <sukvvon@gmail.com>
2024-07-08 23:03:24 +0900

    feat(ui/react/menu): index.ts에 모든 Menu 관련 모듈 추가

---
+++ packages/ui/react/src/components/menu/index.tsx
@@ -0,0 +1,6 @@
+/* eslint-disable @stylistic/padding-line-between-statements, react-refresh/only-export-components */
+export { Menu, type MenuProps } from './menu';
+export { MenuContent, type MenuContentProps } from './menu-content';
+export { MenuPortal, type MenuPortalProps } from './menu-portal';
+export { MenuTrigger, type MenuTriggerProps } from './menu-trigger';
+export { MenuProvider, useMenuContext } from './menu.context';
