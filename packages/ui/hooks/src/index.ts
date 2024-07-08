85bc9693cefb3453ef5a94a35f5445ad5e5dd2a7
Wonsuk Choi <sukvvon@gmail.com>
2024-07-08 22:27:18 +0900

    feat(ui/hooks): index.ts에 useWindowDimension 훅 추가

--- packages/ui/hooks/src/index.ts
+++ packages/ui/hooks/src/index.ts
@@ -3,3 +3,4 @@ export { useBoolean } from './use-boolean';
 export { useDisclosure } from './use-disclosure';
 export { usePosition, type Placement } from './use-position';
 export { useScroll } from './use-scroll';
+export { useWindowDimension } from './use-window-dimension';
