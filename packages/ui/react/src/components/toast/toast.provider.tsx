f6fdc449282c7f9e395a84b347e03a4243d2b16c
Wonsuk Choi <sukvvon@gmail.com>
2024-07-11 16:23:20 +0900

    feat(ui/react/toast): Box 컴포넌트 종속 제거

--- packages/ui/react/src/components/toast/toast.provider.tsx
+++ packages/ui/react/src/components/toast/toast.provider.tsx
@@ -3,7 +3,6 @@ import { useSyncExternalStore } from 'react';
 import { Toast } from './toast';
 import * as styles from './toast.provider.styles.css';
 import { toastStore } from './toast.store';
-import { Box } from '../layout/box';
 import { Portal } from '../portal';
 
 export function ToastProvider() {
@@ -13,12 +12,12 @@ export function ToastProvider() {
   );
 
   const toastList = (
-    <Box className={cx('favolink-toast__list', styles.toastContainerBase)}>
+    <div className={cx('favolink-toast__list', styles.toastContainerBase)}>
       {toastState.map((toast) => (
         <Toast key={toast.id} {...toast} />
       ))}
-    </Box>
+    </div>
   );
 
-  return <Portal type="toast">{toastList}</Portal>;
+  return <Portal>{toastList}</Portal>;
 }
