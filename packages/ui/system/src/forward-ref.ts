f35cc16879a0f0e07614cc384fa245bc084073e1
Wonsuk Choi <sukvvon@gmail.com>
2024-07-11 12:30:01 +0900

    feat(ui/system): forwardRef에 polymorphic 강제성 제거

--- packages/ui/system/src/forward-ref.ts
+++ packages/ui/system/src/forward-ref.ts
@@ -1,24 +1,12 @@
 import {
-  type ComponentPropsWithoutRef,
+  type ElementRef,
   type ElementType,
   type ForwardRefRenderFunction,
   forwardRef as ReactForwardRef,
 } from 'react';
-import {
-  type ComponentWithPolymorphic,
-  type PolymorphicProps,
-  type RightJoinProps,
-} from './types';
 
 export function forwardRef<Props extends object, Component extends ElementType>(
-  render: ForwardRefRenderFunction<
-    any,
-    PolymorphicProps &
-      RightJoinProps<ComponentPropsWithoutRef<Component>, Props>
-  >,
+  render: ForwardRefRenderFunction<ElementRef<Component>, Props>,
 ) {
-  return ReactForwardRef(render) as unknown as ComponentWithPolymorphic<
-    Component,
-    Props
-  >;
+  return ReactForwardRef(render);
 }
