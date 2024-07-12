e243a963dcb8ec3e2df7c03ed00157c90a600064
Wonsuk Choi <sukvvon@gmail.com>
2024-07-12 17:30:27 +0900

    refactor(ui/system/forward-ref.ts): Props를 P, Component를 E로 type generics 단순화

--- packages/ui/system/src/forward-ref.ts
+++ packages/ui/system/src/forward-ref.ts
@@ -5,8 +5,8 @@ import {
   forwardRef as ReactForwardRef,
 } from 'react';
 
-export function forwardRef<Props extends object, Component extends ElementType>(
-  render: ForwardRefRenderFunction<ElementRef<Component>, Props>,
+export function forwardRef<P extends object, E extends ElementType>(
+  render: ForwardRefRenderFunction<ElementRef<E>, P>,
 ) {
   return ReactForwardRef(render);
 }
