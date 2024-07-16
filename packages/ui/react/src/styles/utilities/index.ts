7b25a352f1d4c45665bf5fefdb9e643beaa0ab7c
Wonsuk Choi <sukvvon@gmail.com>
2024-07-16 14:15:15 +0900

    feat(ui/react/utilities/index.ts): color 추가

--- packages/ui/react/src/styles/utilities/index.ts
+++ packages/ui/react/src/styles/utilities/index.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @stylistic/padding-line-between-statements, react-refresh/only-export-components */
 export * from './body.css';
+export * from './color.css';
 export * from './heading.css';
 export * from './letter-spacing.css';
 export * from './text-align.css';
