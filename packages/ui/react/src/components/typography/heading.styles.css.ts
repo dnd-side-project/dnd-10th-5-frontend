f30302d2ab3d8d5a2558b365705ff5a4afe8ee7b
Wonsuk Choi <sukvvon@gmail.com>
2024-07-16 17:17:33 +0900

    refactor(ui/react/typography): typography 관련 컴포넌트 style recipt variants 속성 단축

--- packages/ui/react/src/components/typography/heading.styles.css.ts
+++ packages/ui/react/src/components/typography/heading.styles.css.ts
@@ -9,52 +9,20 @@ import {
 
 export const headingVariants = recipe({
   variants: {
-    align: {
-      left: [textAlign.left],
-      center: [textAlign.center],
-      right: [textAlign.right],
-    },
-    color: {
-      gray100: [color.gray100],
-      gray200: [color.gray200],
-      gray300: [color.gray300],
-      gray400: [color.gray400],
-      gray500: [color.gray500],
-      gray600: [color.gray600],
-      gray700: [color.gray700],
-      gray800: [color.gray800],
-      gray900: [color.gray900],
-      gray1000: [color.gray1000],
-      gray1100: [color.gray1100],
-    },
-    variant: {
-      h1bold: [heading.h1Bold],
-      h1semibold: [heading.h1Semibold],
-      h2bold: [heading.h2Bold],
-      h2semibold: [heading.h2Semibold],
-      h3bold: [heading.h3Bold],
-      h3semibold: [heading.h3Semibold],
-      h4bold: [heading.h4Bold],
-      h4semibold: [heading.h4Semibold],
-      h5bold: [heading.h5Bold],
-      h5semibold: [heading.h5Semibold],
-      h6bold: [heading.h6Bold],
-      h6semibold: [heading.h6Semibold],
-    },
+    align: textAlign,
+    color,
+    variant: heading,
     truncate: {
       true: [truncate],
     },
-    wrap: {
-      wrap: [wrap.wrap],
-      nowrap: [wrap.nowrap],
-    },
+    wrap,
   },
 
   defaultVariants: {
     align: 'left',
     color: 'gray1100',
     truncate: false,
-    variant: 'h1bold',
+    variant: 'h1Bold',
     wrap: 'nowrap',
   },
 });
