f9a2e89a91a2ae8aa9ff173579cbb2493a215af5
Wonsuk Choi <sukvvon@gmail.com>
2024-07-16 22:44:32 +0900

    feat(ui/react/utilities): dynamic, enum이 담긴 index.ts 추가

--- packages/ui/react/src/styles/utilities/index.ts
+++ packages/ui/react/src/styles/utilities/index.ts
@@ -1,8 +1,3 @@
 /* eslint-disable @stylistic/padding-line-between-statements, react-refresh/only-export-components */
-export * from './body.css';
-export * from './color.css';
-export * from './heading.css';
-export * from './letter-spacing.css';
-export * from './text-align.css';
-export * from './truncate.css';
-export * from './wrap.css';
+export * from './dynamic';
+export * from './enum';
