f8cc4d601ad064a04650b820c564ad1e7b4c62ca
Wonsuk Choi <sukvvon@gmail.com>
2024-07-17 22:04:59 +0900

    refactor(apps/web/components/Layout/Header/styles.css.ts): 안정화 되기 전이므로 heading 관련 속성 제거

--- apps/web/src/components/Layout/components/Header/styles.css.ts
+++ apps/web/src/components/Layout/components/Header/styles.css.ts
@@ -1,9 +1,7 @@
-import { heading } from '@favolink-ui/react/utilities.css';
 import { vars } from '@favolink-ui/react/vars.css';
 import { style } from '@vanilla-extract/css';
 
 export const header = style([
-  heading.h1Semibold,
   {
     color: vars.color.archive.coral,
     backgroundColor: vars.color.archive.yellow,
