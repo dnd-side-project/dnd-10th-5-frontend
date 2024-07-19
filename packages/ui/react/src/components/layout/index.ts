b52baaf61029b4cb7620fcf1055105864920d7c1
Wonsuk Choi <sukvvon@gmail.com>
2024-07-20 00:47:17 +0900

    refactor(ui/react/components/layout): Flex 컴포넌트 개편, createCssPropertyStyleVariants 함수 삭제 (#256)
    * feat(ui/react/components/layout): flex.styles.css.ts 삭제, dynamic, enum 속성이 포함된 Flex 컴포넌트 스타일 추가
    
    * refactor(ui/react/components/layout): dynamic, enum 속성이 포함된 Flex 컴포넌트로 개편
    
    * feat(ui/react/components/layout): 사용하지 않는 createCssPropertyStyleVariants 삭제

--- packages/ui/react/src/components/layout/index.ts
+++ packages/ui/react/src/components/layout/index.ts
@@ -1,8 +1,3 @@
 /* eslint-disable @stylistic/padding-line-between-statements, react-refresh/only-export-components */
-export {
-  createCSSPropertyStyleVariants,
-  type CSSPropertyValue,
-} from './create-css-property-style-variants';
-
 export { Box, type BoxProps } from './box';
 export { Flex, type FlexProps } from './flex';
