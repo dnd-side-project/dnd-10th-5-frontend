95a29588c65ee2122de236a410efbd1e810dd34f
Wonsuk Choi <sukvvon@gmail.com>
2024-07-19 21:05:11 +0900

    feat(ui/react/styles): extractDynamicProps 함수 index.ts에 추가 (#252)

--- packages/ui/react/src/styles/index.ts
+++ packages/ui/react/src/styles/index.ts
@@ -1,3 +1,4 @@
 /* eslint-disable @stylistic/padding-line-between-statements, react-refresh/only-export-components */
 export * from './props';
 export * from './vars';
+export * from './extract-dynamic-props';
