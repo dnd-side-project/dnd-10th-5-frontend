20c6767f0380bd8041af1409a1923f76c784cd70
Wonsuk Choi <sukvvon@gmail.com>
2024-07-23 17:14:34 +0900

    refactor(apps/web): @favolink-ui/styles 추가 및 변경사항 반영

--- apps/web/src/components/Layout/components/Header/styles.css.ts
+++ apps/web/src/components/Layout/components/Header/styles.css.ts
@@ -1,4 +1,4 @@
-import { vars } from '@favolink-ui/react/theme.css';
+import { vars } from '@favolink-ui/styles/theme.css';
 import { style } from '@vanilla-extract/css';
 
 export const header = style([
