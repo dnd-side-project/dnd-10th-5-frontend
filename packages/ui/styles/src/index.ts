10a78204898ae2ad158d8446ff9988071112d630
Wonsuk Choi <sukvvon@gmail.com>
2024-07-25 21:44:35 +0900

    feat(ui/styles): index.ts에 mapToProp, make(Custom)Data 추가

--- packages/ui/styles/src/index.ts
+++ packages/ui/styles/src/index.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @stylistic/padding-line-between-statements, react-refresh/only-export-components */
 export * from './utils';
 export * from './extract-dynamic-props';
-export * from './make-style-variants-data';
+export * from './make-data';
+export * from './map-to-prop';
 export * from './theme.css';
