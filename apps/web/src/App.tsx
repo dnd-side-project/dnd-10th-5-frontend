fd7e608e1bf8831d28c1d8282951c7a63b6047e9
Wonsuk Choi <sukvvon@gmail.com>
2024-07-27 15:56:01 +0900

    refactor(apps/web): App.tsx에 @favolink-ui/react/reset.css로 경로 변경

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -3,7 +3,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
-import '@favolink-ui/styles/reset.css';
+import '@favolink-ui/react/reset.css';
 import '@favolink-ui/react/theme.css';
 import '@/styles/font.css';
 
