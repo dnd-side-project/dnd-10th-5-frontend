2c5db7f37b849a34cd39b82c33a773a979e7693f
Wonsuk Choi <sukvvon@gmail.com>
2024-07-27 15:00:02 +0900

    fix(apps/web): Layout Header styles.css.ts globalVars 경로 수정

--- apps/web/src/components/Layout/components/Header/styles.css.ts
+++ apps/web/src/components/Layout/components/Header/styles.css.ts
@@ -1,4 +1,4 @@
-import { globalVars } from '@favolink-ui/react';
+import { globalVars } from '@favolink-ui/react/theme.css';
 import { style } from '@vanilla-extract/css';
 
 export const header = style([
