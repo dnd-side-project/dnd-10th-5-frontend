86d61b47140d118c1451ebdd71b910595270969d
Wonsuk Choi <sukvvon@gmail.com>
2024-08-01 16:32:49 +0900

    feat(ui/react): extractMarginProps, margin 관련 style 추가

---
+++ packages/ui/react/src/margin/margin.css.ts
@@ -0,0 +1,45 @@
+import { createThemeContract, style } from '@vanilla-extract/css';
+
+export const marginVars = createThemeContract({
+  margin: null,
+  marginX: null,
+  marginY: null,
+  marginTop: null,
+  marginRight: null,
+  marginBottom: null,
+  marginLeft: null,
+});
+
+export const margin = style({
+  margin: marginVars.margin,
+});
+
+export const marginX = style({
+  marginRight: marginVars.marginX,
+  marginLeft: marginVars.marginX,
+});
+
+export const marginY = style({
+  marginRight: marginVars.marginX,
+  marginLeft: marginVars.marginX,
+});
+
+export const marginTop = style({
+  marginTop: marginVars.marginTop,
+});
+
+export const marginRight = style({
+  marginRight: marginVars.marginRight,
+});
+
+export const marginBottom = style({
+  marginBottom: marginVars.marginBottom,
+});
+
+export const marginLeft = style({
+  marginLeft: marginVars.marginLeft,
+});
+
+export type MarginVariants = {
+  [K in keyof typeof marginVars]?: number | string;
+};
