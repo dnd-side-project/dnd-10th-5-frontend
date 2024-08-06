d608fd3cb26dc9f70f1930d132f601c621014d26
Wonsuk Choi <sukvvon@gmail.com>
2024-08-06 15:25:01 +0900

    feat(ui/hooks): usePrevious hook 추가

---
+++ packages/ui/src/hooks/use-previous.ts
@@ -0,0 +1,14 @@
+import { useMemo, useRef } from 'react';
+
+export function usePrevious<T>(value: T) {
+  const ref = useRef({ value, previous: value });
+
+  return useMemo(() => {
+    if (ref.current.value !== value) {
+      ref.current.previous = ref.current.value;
+      ref.current.value = value;
+    }
+
+    return ref.current.previous;
+  }, [value]);
+}
