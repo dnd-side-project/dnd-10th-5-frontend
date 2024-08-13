4fd9853eebf467c6b250ac277d22010fbf3ab9ec
Wonsuk Choi <sukvvon@gmail.com>
2024-08-14 00:05:23 +0900

    refactor(ui/utils): mergeFns의 반환값 함수 선언으로 변경 (#307)

--- packages/ui/src/utils/merge-fns.ts
+++ packages/ui/src/utils/merge-fns.ts
@@ -1,7 +1,7 @@
-type AnyFunction<T = any> = (...args: T[]) => any;
-
-export function mergeFns<T extends AnyFunction>(...fns: (T | undefined)[]) {
-  return (...args: Parameters<T>) => {
+export function mergeFns<T extends (...args: any[]) => any>(
+  ...fns: (T | undefined)[]
+) {
+  return function mergedFns(...args: Parameters<T>) {
     fns.forEach((fn) => {
       fn?.(...args);
     });
