974e3ded4954c2ee8ee5d4490f6856e7a18cb16e
Wonsuk Choi <sukvvon@gmail.com>
2024-08-16 12:37:52 +0900

    feat(ui/styles): makeData의 key를 케밥 케이스로 변경

--- packages/ui/src/styles/make-data.ts
+++ packages/ui/src/styles/make-data.ts
@@ -1,19 +1,9 @@
 import { type ComplexStyleRule } from '@vanilla-extract/css';
 
-type KebabToCamelCase<T extends string> = T extends `${infer T1}-${infer T2}`
-  ? `${Lowercase<T1>}${Capitalize<KebabToCamelCase<T2>>}`
-  : T;
-
-function kebabToCamelCase<T extends string>(value: T) {
-  return value.replace(/-./g, (match) =>
-    match.charAt(1).toUpperCase(),
-  ) as KebabToCamelCase<T>;
-}
-
 export function makeData<T extends readonly string[]>(dataValues: T) {
   const data = dataValues.reduce(
-    (prevData, value) => ({ ...prevData, [kebabToCamelCase(value)]: value }),
-    {} as Record<KebabToCamelCase<T[number]>, T[number]>,
+    (prevData, value) => ({ ...prevData, [value]: value }),
+    {} as Record<T[number], T[number]>,
   );
 
   return data;
