34a59e4c37e95d6aba373ec48b6ef2e8817a6d30
Wonsuk Choi <sukvvon@gmail.com>
2024-08-24 03:20:23 +0900

    feat(ui/components/layout): box 관련 style 추가

--- packages/ui/src/components/layout/box.css.ts
+++ packages/ui/src/components/layout/box.css.ts
@@ -1,130 +1,18 @@
-import { createThemeContract, style } from '@vanilla-extract/css';
 import { type RecipeVariants, recipe } from '@vanilla-extract/recipes';
-import {
-  flexGrow,
-  flexShrink,
-  overflow,
-  overflowX,
-  overflowY,
-  position,
-} from '../../styles';
+import { display } from '../../styles';
 
-export const dynamicVars = createThemeContract({
-  padding: null,
-  paddingX: null,
-  paddingY: null,
-  paddingTop: null,
-  paddingRight: null,
-  paddingBottom: null,
-  paddingLeft: null,
-  width: null,
-  minWidth: null,
-  maxWidth: null,
-  height: null,
-  minHeight: null,
-  maxHeight: null,
-  inset: null,
-  top: null,
-  right: null,
-  bottom: null,
-  left: null,
-  flexBasis: null,
-});
-
-export const padding = style({
-  padding: dynamicVars.padding,
-});
-
-export const paddingX = style({
-  paddingRight: dynamicVars.paddingX,
-  paddingLeft: dynamicVars.paddingX,
-});
-
-export const paddingY = style({
-  paddingRight: dynamicVars.paddingY,
-  paddingLeft: dynamicVars.paddingY,
-});
-
-export const paddingTop = style({
-  paddingTop: dynamicVars.paddingTop,
-});
-
-export const paddingRight = style({
-  paddingRight: dynamicVars.paddingRight,
-});
-
-export const paddingBottom = style({
-  paddingBottom: dynamicVars.paddingBottom,
-});
-
-export const paddingLeft = style({
-  paddingLeft: dynamicVars.paddingLeft,
-});
-
-export const width = style({
-  width: dynamicVars.width,
-});
-
-export const minWidth = style({
-  minHeight: dynamicVars.minWidth,
-});
-
-export const maxWidth = style({
-  maxHeight: dynamicVars.maxWidth,
-});
-
-export const height = style({
-  height: dynamicVars.height,
-});
-
-export const minHeight = style({
-  minHeight: dynamicVars.minHeight,
-});
-
-export const maxHeight = style({
-  maxHeight: dynamicVars.maxHeight,
-});
-
-export const inset = style({
-  inset: dynamicVars.inset,
-});
-
-export const top = style({
-  top: dynamicVars.top,
-});
-
-export const right = style({
-  right: dynamicVars.right,
-});
-
-export const bottom = style({
-  bottom: dynamicVars.bottom,
-});
-
-export const left = style({
-  left: dynamicVars.left,
-});
-
-export const flexBasis = style({
-  flexBasis: dynamicVars.flexBasis,
-});
-
-export type BoxDynamicVariants = {
-  [K in keyof typeof dynamicVars]?: number | string;
-};
-
-export const boxEnumVariants = recipe({
+export const boxVariants = recipe({
   variants: {
-    position,
-    overflow,
-    overflowX,
-    overflowY,
-    flexShrink,
-    flexGrow,
+    display: {
+      none: display.none,
+      inline: display.inline,
+      'inline-block': display['inline-block'],
+      block: display.block,
+    },
   },
 });
 
-export type BoxEnumVariants = Exclude<
-  RecipeVariants<typeof boxEnumVariants>,
+export type BoxVariants = Exclude<
+  RecipeVariants<typeof boxVariants>,
   undefined
 >;
